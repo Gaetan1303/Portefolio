4c9296065f3ca92f29766fffcda6c183568af9f7
Gaetan1303 <gaetan.begue13@gmail.com>
2025-06-30 13:58:17 +0200

    Ajout

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ var header = document.getElementById("sticky-header");
 var sticky = header.offsetTop;
 
 function stickHeader() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky-header");
-  } else {
-    header.classList.remove("sticky-header");
-  }}
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky-header");
+    } else {
+        header.classList.remove("sticky-header");
+    }
+}
+
+// Toggle function for hiding and showing contact information
+function toggleContactInfo() {
+    var contactInfo = document.getElementById("contact-info");
+    // Toggle the 'hide' class on the contact info
+    contactInfo.classList.toggle("hide");
+}
